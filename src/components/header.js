10e6b05c3fa097be7e1e7b8bb84efa3f84cb7ec5
juanjuanzero-dev <juanjuanzero.dev@gmail.com>
2023-11-23 08:57:52 -0500

    pivoting, to a smaller brand name

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,14 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <nav className="flex bg-blue-300 py-4 px-2 justify-between">
-      <div><Link className="text-3xl" to="/" >
+    <nav className="flex shadow-md py-4 px-2 justify-between">
+      <div><Link className="text-xl bg-red-500" to="/" >
         {siteTitle}
       </Link></div>
       <div className="flex gap-4">
-        <Link className="bg-blue-200" to="#services">Services</Link>
-        <Link className="bg-blue-200" to="#testimonials">Testimonials</Link>
-        <Link className="bg-blue-200" to="#contact">Contact</Link>
+        <Link className="bg-red-500" to="#services">Services</Link>
+        <Link className="bg-red-500" to="#testimonials">Testimonials</Link>
+        <Link className="bg-red-500" to="#contact">Contact</Link>
       </div>
     </nav>
 
